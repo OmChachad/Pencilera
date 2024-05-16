0e0c634aa0461d7ee076b14ae50f1146487dd46e
Om Chachad <96822675+iTechEverything@users.noreply.github.com>
2024-05-16 21:47:42 +0530

    Removed UIAppearance Code

--- Pencilera.swiftpm/App/CameraApp.swift
+++ Pencilera.swiftpm/App/CameraApp.swift
@@ -6,25 +6,9 @@ import SwiftUI
 
 @main
 struct CameraApp: App {
-
-    init() {
-        UINavigationBar.applyCustomAppearance()
-    }
-    
     var body: some Scene {
         WindowGroup {
             CameraView()
         }
-    }
-}
-
-fileprivate extension UINavigationBar {
-    
-    static func applyCustomAppearance() {
-        let appearance = UINavigationBarAppearance()
-        appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
-        UINavigationBar.appearance().standardAppearance = appearance
-        UINavigationBar.appearance().compactAppearance = appearance
-        UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
 }
