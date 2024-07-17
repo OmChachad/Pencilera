0d1d964981bce48be1a3154e51a31bf96159516d
Om Chachad <96822675+iTechEverything@users.noreply.github.com>
2024-07-17 23:20:03 +0530

    Removed unused code

--- Pencilera.swiftpm/App/CameraView.swift
+++ Pencilera.swiftpm/App/CameraView.swift
@@ -147,25 +147,3 @@ struct CameraView: View {
         }
     }
 }
-
-//struct FlipTransition: ViewModifier, Animatable {
-//    var progress: CGFloat = 0
-//    var animatableData: CGFloat {
-//        get { progress }
-//        set { progress = newValue }
-//    }
-//    func body(content: Content) -> some View {
-//        content
-//            .opacity(progress < 0 ? (-progress < 0.5 ? 1 : 0) : (progress < 0.5 ? 1 : 0))
-//            .rotation3DEffect(.init(degrees: progress * 180), axis: (x: 0.0, y: 1.0, z: 0.0))
-//    }
-//    
-//}
-//
-//extension AnyTransition {
-//    static let flip: AnyTransition = modifier(
-//        active: FlipTransition (progress: -1), identity: FlipTransition()
-//    )
-//    static let reverseFlip: AnyTransition = modifier(
-//        active: FlipTransition (progress: 1), identity: FlipTransition())
-//}
