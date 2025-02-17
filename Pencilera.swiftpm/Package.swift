9e449fc8dd823c76e90162e2c1679625bfc6251a
Om Chachad <96822675+OmChachad@users.noreply.github.com>
2025-02-17 21:31:46 +0530

    Updated Version Number

--- Pencilera.swiftpm/Package.swift
+++ Pencilera.swiftpm/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
             targets: ["App"],
             bundleIdentifier: "org.starlightapps.pencilera",
             teamIdentifier: "3S6NT5MUQZ",
-            displayVersion: "2.0",
+            displayVersion: "2.1",
             bundleVersion: "15",
             appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.blue),
